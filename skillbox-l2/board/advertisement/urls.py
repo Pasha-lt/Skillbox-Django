f2eb748a904c44fde70aa4d0f4e7660025e19549
Pasha <9139136@gmail.com>
2020-07-08 12:16:23 +0300

    according to requirements

--- skillbox-l2/board/advertisement/urls.py
+++ skillbox-l2/board/advertisement/urls.py
@@ -2,5 +2,10 @@ from .import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.advertisement_list, name='advertisement_list')
+    path('', views.advertisement_list, name='advertisement_list'),
+    path('course1/', views.course1, name='course1'),
+    path('course2/', views.course2, name='course2'),
+    path('course3/', views.course3, name='course3'),
+    path('course4/', views.course4, name='course4'),
+    path('course5/', views.course5, name='course5'),
 ]
--- skillbox-l2/board/advertisement/views.py
+++ skillbox-l2/board/advertisement/views.py
@@ -4,3 +4,23 @@ from django.http import HttpResponse
 
 def advertisement_list(request, *args, **kwargs):
     return render(request, 'advertisement/advertisement_list.html', {})
+
+
+def course1(request, *args, **kwargs):
+    return render(request, 'advertisement/course1.html', {})
+
+
+def course2(request, *args, **kwargs):
+    return render(request, 'advertisement/course2.html', {})
+
+
+def course3(request, *args, **kwargs):
+    return render(request, 'advertisement/course3.html', {})
+
+
+def course4(request, *args, **kwargs):
+    return render(request, 'advertisement/course4.html', {})
+
+
+def course5(request, *args, **kwargs):
+    return render(request, 'advertisement/course5.html', {})
